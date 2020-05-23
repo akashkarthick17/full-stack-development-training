c69af027e245fec546878ebed3c1d533c65f65c5
Akash Karthick <akarthick@nvenergy.com>
2020-05-23 21:38:49 +0530

    day-7 material

---
+++ day-7/basics.js
@@ -0,0 +1,25 @@
+// Declare a variable;
+var test = 1234;
+
+console.log(test);
+
+function sampleFunction() {
+  console.log('sample function called...');
+}
+
+// Function call.
+sampleFunction();
+
+// Condition.
+if (test > 1000) {
+  console.log('Greater than 1000...');
+} else {
+  console.log('Less than 1000...');
+}
+
+// Loops
+for (var i = 0; i < 10; i++) {
+  console.log('loop: ' + i);
+}
+
+
---
+++ day-7/hoisting.js
@@ -0,0 +1,18 @@
+
+// Hoisting.
+
+console.log(var1); // undefined
+test(); // prints value
+test2(); // throws an error
+
+
+var var1 = "Hello world";
+
+function test() {
+  console.log("test function called");
+}
+
+var test2 = function() {
+  console.log("test2 function called");
+}
+
